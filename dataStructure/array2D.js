b4f69a07e29c6ccd4cc2dd3aa81069e7ea8c8dff
Anjalik19 <anjali.march19@gmail.com>
2019-12-26 12:53:06 +0530

    [ANJALI] Refactor.Business logic of 2D array program

---
+++ dataStructure/array2D.js
@@ -0,0 +1,4 @@
+const readline = require("readline-sync");
+let util = require("../utility/dataStructureUtility");
+let n;
+let result=util.array2D(n);
--- utility/dataStructureUtility.js
+++ utility/dataStructureUtility.js
@@ -285,7 +285,7 @@ exports.balancedParentheses = (string) => {
  * @return true if it is a palindrome or return false.
  */
 exports.palindrome=(str)=>
-{
+{ 
          let deque = new deque();
         for (let i=str.length-1;i>=0;i--) {
             deque.addFirst(str.charAt(i));
@@ -303,4 +303,34 @@ exports.palindrome=(str)=>
         }
 
     }
+    
+    exports.array2D=(n)=>
+    {
+        function isPrime(n) {
+            if (n == 0 || n == 1) {
+                return false;
+              }
+              for (let i = 2; i < n; i++) {
+                if (n % i == 0) {
+                  return false;
+                }
+              }
+              return true;
+            }
+            let a=[];
+            let k = 0;
+            for (let i = 0; i < 10; i++) {
+                a[i]=[10];
+              for (let j = 0; j < 100; j++) {
+                  a[i][j]=[10][100];
+                if (isPrime(k)) {
+                  console.log("" + k);
+                  k++;
+                } else {
+                  k++;
+                }
+              }
+              console.log(" ");
+                }
+            }
     
