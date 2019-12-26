8eadabaa8fbc55c136eb8db4dd761738d9def928
Anjalik19 <anjali.march19@gmail.com>
2019-12-26 12:03:40 +0530

    [ANJALI] Refactor.Data Structure program

--- dataStructure/balancedParentheses.js
+++ dataStructure/balancedParentheses.js
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Execution     : 1. default node cmd> node balancedParentheses.js
-*                 2. if nodemon installed cmd> node balancedParentheses.js
+*                 2. if nodemon installed cmd> nodemon balancedParentheses.js
 * 
 * Purpose       : Program to Show Arithmetic Expression is balanced or not.
 * 
