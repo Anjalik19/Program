a5f0dba0ef5db5fff193754b689e1b01a7b83b22
Anjalik19 <anjali.march19@gmail.com>
2019-12-26 14:49:30 +0530

    [ANJALI] Refactor.Business logic of calander program

--- dataStructure/calander.js
+++ dataStructure/calander.js
@@ -1,3 +1,19 @@
+/******************************************************************************
+* Execution     : 1. default node cmd> node calander.js
+*                 2. if nodemon installed cmd> nodemon calander.js
+* 
+* Purpose       : Takes the month and year as input and prints the Calendar of the month. 
+* 
+* @description 
+* 
+* @file         : calander.js
+* @overview     : Takes the month and yearas input and prints the Calendar of the month. 
+* @module       : calander - This is optional if expeclictly its an npm or local package
+* @author       : Anjali<anjali.march19@gmail.com>
+* @version      : 1.0
+* @since        : 26-11-2019
+*
+******************************************************************************/
 const readline = require("readline-sync");
 let util = require("../utility/dataStructureUtility");
-util.calander()
+util.calander();
