35df838e54959215685c0950d4f899faf055ce8e
Anjalik19 <anjali.march19@gmail.com>
2019-12-28 15:26:15 +0530

    [ANJALI]Created json file and stock management program

--- objectOrientedProgram/stockManagement.js
+++ objectOrientedProgram/stockManagement.js
@@ -4,3 +4,12 @@ let content=fs.readFileSync("./stock.json");
 let data=JSON.parse(content);
 console.log(data);
 let length=data.stock_report.length;
+let count=0;
+for(let i=0;i<length;i++)
+{
+    let name=data.stock_report[i].stock_name;
+    let price=data.stock_report[i].share_price;
+    console.log("price of each stock:",name,"=",price);
+    count=count+data.stock_report[i].share_price;
+}
+console.log("Total price of share:",count);
