2b4e42f584801091d7700c3dbcb4b612b2bc5b77
Anjalik19 <anjali.march19@gmail.com>
2019-12-28 14:29:10 +0530

    [ANJALI]Created Business logic for json file

--- objectOrientedProgram/inventoryDataManagement.js
+++ objectOrientedProgram/inventoryDataManagement.js
@@ -4,10 +4,37 @@ let content=fs.readFileSync('./inventory.json');
 let data = JSON.parse(content);
 console.log(data);
 let length=data.Rice.length;
+let count=0;
+console.log("-----------Rice--------------");
 for(let i=0;i<length;i++)
 {
-    let name=data.Rice[i].Name;
-    let weight=data.Rice[i].Weight;
+    let name=data.Rice[i].name;
+    let weight=data.Rice[i].weight;
     let price=data.Rice[i].price;
-    console.log("Total price of rice:",price);
+    count+=data.Rice[i].price;
+    console.log("price of each rice is:",price);
 }
+console.log("Total price of rice is:",count);
+console.log("-----------Pulses--------------");
+let lengths=data.Pulses.length;
+count=0;
+for(let j=0;j<lengths;j++)
+{
+    let name=data.Pulses[j].name;
+    let weigth=data.Pulses[j].weight;
+    let price=data.Pulses[j].price;
+    count=count+data.Pulses[j].price;
+    console.log("price of each pulses is:",price);
+}
+console.log("Total price of pulses is:",count);
+console.log("-----------Wheat-------------");
+let leng=data.Wheats.leng;
+for(let k=0;k<length;k++)
+{
+let name=data.Wheats[k].name;
+let weight=data.Wheats[k].weight;
+let price=data.Wheats[k].price;
+count+=count+data.Wheats[k].price;
+console.log("price of each weight is:",price);
+}
+console.log("Total price of wheats is:",count);
