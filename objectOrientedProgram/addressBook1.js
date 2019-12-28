e02f70a4839947f431627a67bea2821d8e5325d8
Anjalik19 <anjali.march19@gmail.com>
2019-12-28 18:14:25 +0530

    [ANJALI]Refactor.Business logic of address book program to some extent

--- objectOrientedProgram/addressBook1.js
+++ objectOrientedProgram/addressBook1.js
@@ -1,33 +1,30 @@
 let readline = require("readline-sync")
 let fs = require('fs')
-class address
-{
-constructor(addressBook)
-{
-    this.addressBook;
-}
-newAddress=(addressbook)=>
-{
-    let name = read.question("enter first name:");
-    let lastname = read.question("enter last name:");
-    let address=readline.question("enter the address:")
-    let state = read.question("enter the state:");
-    let city = read.question("enter the city:");
-    let pincode = read.question("enter the pincode:");
-    let phonenumber = read.question("enter your phonenumber:");
-    addressBook.address_book.push(
-        {
-            "first_name":name,
-            "last_name":lastname,
-            "address":address,
-            "state":state,
-            "city":city,
-            "pincode":pincode,
-            "phone_number":phonenumber
-        });
-       
+class address {
+    constructor(addressBook) {
+        this.addressBook;
     }
-    writeFile=(fileName, data)=> {
+    newAddress = (addressbook) => {
+        let name = read.question("enter first name:");
+        let lastname = read.question("enter last name:");
+        let address = readline.question("enter the address:")
+        let state = read.question("enter the state:");
+        let city = read.question("enter the city:");
+        let pincode = read.question("enter the pincode:");
+        let phonenumber = read.question("enter your phonenumber:");
+        addressBook.address_book.push(
+            {
+                "first_name": name,
+                "last_name": lastname,
+                "address": address,
+                "state": state,
+                "city": city,
+                "pincode": pincode,
+                "phone_number": phonenumber
+            });
+
+    }
+    writeFile = (fileName, data) => {
         let fs = require('fs');
         fs.writeFile(addressBook.json, JSON.stringify(addressBook), function (err) {
             if (err) {
@@ -36,7 +33,7 @@ newAddress=(addressbook)=>
         });
         console.log("*******Address update succesfully******");
         console.log("address information as per record:\nfirstName:  ${name}   \nlastname: ${lastname}  \naddress: ${address} \ncity: ${city}  \nstate ${state} \npincode: ${zip}  \nphone number: ${phonenumber}");
-        return [name, lastname,address,state, city, pincode, phonenumber];
+        return [name, lastname, address, state, city, pincode, phonenumber];
     }
     showDetails(addressBook) {
         if (addressBook.address_book.length > 0) {
@@ -44,9 +41,23 @@ newAddress=(addressbook)=>
                 console.log(addressBook.address_book[i]);
             }
         }
-       
+
     }
-    
+remove=()=>
+{
+    let delete1 = read.question("Please enter the index you want to delete: ");
+    delete addressBook.address_book[delete1];
+    for (let i = 0; i < addressBook.address_book.length; i++) {
+        if (addressBook.address_book[i] == null) {
+            addressBook.address_book.splice(i, 1);
+        }
+    }
+    file.writeFile('addressBook.json', JSON.stringify(addressBook), function (err) {
+        if (err) throw err
+        console.log('Done!')
+    })
+}
+
 }
 
 
