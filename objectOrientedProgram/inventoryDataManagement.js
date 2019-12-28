dfd411f496a45243e3da9cf16ff42a364ac586a7
Anjalik19 <anjali.march19@gmail.com>
2019-12-28 12:58:16 +0530

    [ANJALI]Created Business logic for json file

--- objectOrientedProgram/inventoryDataManagement.js
+++ objectOrientedProgram/inventoryDataManagement.js
@@ -3,4 +3,11 @@ const fs = require("fs");
 let content=fs.readFileSync('./inventory.json');
 let data = JSON.parse(content);
 console.log(data);
-f
+let length=data.Rice.length;
+for(let i=0;i<length;i++)
+{
+    let name=data.Rice[i].Name;
+    let weight=data.Rice[i].Weight;
+    let price=data.Rice[i].price;
+    console.log("Total price of rice:",price);
+}
