2d08d813e4fcf2b1f1fc1e1512dbf3493b0d01c8
Anjalik19 <anjali.march19@gmail.com>
2019-12-31 16:20:31 +0530

    [Anjali]Refactor factory design pattern

--- objectOrientedProgram/designPattern/factoryDesign.js
+++ objectOrientedProgram/designPattern/factoryDesign.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+* Execution : 1. default node cmd> node factoryDesignPattern.js
+*             2. if nodemon installed cmd> nodemon factoryDesignPattern.js
+* 
+* Purpose   : program to create computer Factory that can Produce PC, Laptop
+* @description 
+* 
+* @file     : factoryDesignPattern.js
+* @overview : program to create computer Factory that can Produce PC, Laptop
+* @module   : factoryDesignPattern- This is optional if expeclictly its an npm or local package
+* @author   : Anjali<anjali.march19@gmail.com>
+* @version  : 1.0
+* @since    : 31-11-2019
+*
+******************************************************************************/
 function pc(name)
 {
 this.name=name;
@@ -7,6 +22,11 @@ function laptop(name)
 {
     this.name=name;
     this.type="laptop";
+}
+function server(name)
+{
+    this.name=name;
+    this.type="server";
 }
 function computerFactory()
 {
@@ -18,20 +38,22 @@ switch(type)
     break;
     case 2:return new laptop(name);
     break;
+    case 3:return new server(name);
 }
     }
 }
 function show()
 {
-    console.log("hello..i am",this.name,"and i am a",this.type);
+    console.log("hello..i am",this.name,"and i am a",this.type,);
 }
 const factory=new computerFactory();
 let arr=[];
 arr.push(factory.create("Zebronic",1));
-arr.push(factory.create("aser",2));
-arr.push(factory.create("viao",2));
+arr.push(factory.create("aser",2,));
+arr.push(factory.create("viao",2,));
 arr.push(factory.create("hp",1));
-
-arr.forEach(emp => {
-    show.call(emp)
+arr.push(factory.create("Proxy server",3));
+arr.push(factory.create("web server",3));
+arr.forEach(comp => {
+    show.call(comp)
 });
