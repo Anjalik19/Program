767741912b6506b95a83e8e1a724c705ca300489
Anjalik19 <anjali.march19@gmail.com>
2019-12-31 18:12:00 +0530

    [Anjali]Refactor.Business logic of  prototype design pattern

--- designPattern/prototypeDesignPattern.js
+++ designPattern/prototypeDesignPattern.js
@@ -15,4 +15,18 @@ class Name{
             clone[i] = this.array[i];
         }
         return clone;
-   
+    }
+}
+let  prototype = () => {
+    let arr1 = [];
+    let arr2 = [];
+    let a = new Name();
+    a.data();
+    arr1 = a.clone();
+    arr2 = a.clone();
+    arr1.push("anuj");
+    arr2.push("akash");
+    console.log(arr1);
+    console.log(arr2);
+}
+prototype();
