9728c40a32d6ac4529080070818358e09b2737bd
Anjalik19 <anjali.march19@gmail.com>
2019-12-31 17:42:32 +0530

    [Anjali]Refactor observer design pattern

--- designPattern/observerDesignPattern.js
+++ designPattern/observerDesignPattern.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+* Execution : 1. default node cmd> node observerDesignPattern.js
+*             2. if nodemon installed cmd> nodemon observerDesignPattern.js
+* 
+* Purpose   : program to create cObserver design pattern.
+* @description 
+* 
+* @file     : observerDesignPattern.js
+* @overview : program to create cObserver design pattern.
+* @module   : observerDesignPattern- This is optional if expeclictly its an npm or local package
+* @author   : Anjali<anjali.march19@gmail.com>
+* @version  : 1.0
+* @since    : 31-11-2019
+*
+******************************************************************************/
 function subject()
 {
   this.observers=[];
