231367a812913fdb1b961a256cf5d9ef5a856006
Anjalik19 <anjali.march19@gmail.com>
2019-12-31 19:03:11 +0530

    [Anjali]Refactor.Business logic of observer design pattern

--- designPattern/singletonDesignPattern.js
+++ designPattern/singletonDesignPattern.js
@@ -13,28 +13,12 @@
 * @since    : 31-11-2019
 *
 ******************************************************************************/
-let singleton = (function () {
-    let instance;
-    function createInstance() {
-        let object = new Object("hello");
-        return object;
-    }
-    return {
-        getInstance: function () {
-            if (!instance) {
-                instance = createInstance();
-            }
-            return instance;
-        }
-    };
-})();
-function run() {
-    let instance1 = singleton.getInstance();
-    let instance2 = singleton.getInstance();
-    if (instance1 === instance2) {
-        console.log("true");
-    } else {
-        console.log("false");
-    }
+const readline=require("readline-sync");
+let util=require("../utility/designPatternUtility");
+try{
+    util.singletonDesign();
 }
-run();
+catch(err)
+{
+    console.log(err);
+}
